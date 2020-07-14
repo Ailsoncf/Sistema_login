faca154eed634422ca4524afe544f66651d5f8ca
Ailsoncf <ailson_cf@yahoo.com.br>
2020-07-14 18:34:21 +1200

    Adding bcrypt

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,14 +1,34 @@
 const User = require("../models/User")
+const bcrypt = require("bcryptjs")
 
 module.exports = {
   async signUp(request, response) {
     const { name, email, password } = request.body
 
-    const user = await User.create({
-      name,
-      email,
-      password,
-    })
-    return response.json(user)
+    const emailExists = await User.findOne({ email })
+
+    try {
+      if (emailExists)
+        return response
+          .status(409)
+          .json({ error: "Email already exists, try another one!" })
+
+      const hash = await bcrypt.hash(password, 10)
+      const user = await User.create({
+        name,
+        email,
+        password: hash,
+      })
+
+      user.password = undefined
+
+      return response.json(user)
+    } catch (err) {
+      return response.status(406).json({ error: "Registration Failed" })
+    }
+  },
+
+  async signIn(request, response) {
+    return response.json({ message: "funcionando" })
   },
 }
--- backend/routes.js
+++ backend/routes.js
@@ -6,5 +6,6 @@ const routes = Router()
 const AuthController = require("./controllers/AuthController")
 
 routes.post("/register", AuthController.signUp)
+routes.get("/register", AuthController.signIn)
 
 module.exports = routes
