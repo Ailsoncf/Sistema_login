ef39b9806081ed2495c35dd7b4f66fd557ddd0a6
Ailsoncf <ailson_cf@yahoo.com.br>
2020-07-15 12:13:13 +1200

    Adding bcrypt

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -14,6 +14,7 @@ module.exports = {
           .json({ error: 'Email already exists, try another one!' })
 
       const hash = await bcrypt.hash(password, 10)
+
       const user = await User.create({
         name,
         email,
